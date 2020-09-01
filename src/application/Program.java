b824f1b109eb1ed6fe9115ebb10e56c64c976fac
caique <mongodb@oc1016435322.ibm.com>
2020-09-01 11:43:55 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		Position pos = new Position(5, 3);
+		System.out.println(pos);
 
 	}
 
